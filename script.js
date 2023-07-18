402a343bf0fef64ef9abc02607d592284b7f701f
Reham KamalEldin <115808357+Rehamzahra@users.noreply.github.com>
2023-07-19 00:23:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let doller = document.getElementById('doller');
+let pound = document.getElementById('pound');
+
+
+function calc1(){
+
+    pound.value = doller.value * 30;
+
+}
+
+function calc2(){
+    
+    doller.value = pound.value /30;
+
+}
+
+
